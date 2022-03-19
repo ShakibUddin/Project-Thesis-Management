feb30f342878d478277430d36b12f4e3eef6683c
ShakibUddin <shakibUddinbhuiyan@gmail.com>
2022-03-19 17:00:32 +0600

    signup, signin ui done, added main layout,set up router and connected to signup,signin and mainlayout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/antd.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./Containers/Login/LoginPage";
+import MainLayout from './Containers/Main/MainLayout';
+import SignupPage from "./Containers/Signup/SignupPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="mainlayout" element={<MainLayout />} />
+          <Route path="signin" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
